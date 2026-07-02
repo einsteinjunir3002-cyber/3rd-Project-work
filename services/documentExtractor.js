756a04e8ae2b1e7c99fb3899fc7d068b66211efb
Samuel Bekoe <274854852+einsteinjunir3002-cyber@users.noreply.github.com>
2026-07-02 07:01:03 +0000

    fix: polyfill DOMMatrix for pdf-parse to prevent Vercel boot crash

--- services/documentExtractor.js
+++ services/documentExtractor.js
@@ -1,3 +1,14 @@
+// Polyfill browser globals that pdf-parse / pdfjs-dist expects in Node.js environments
+if (typeof global.DOMMatrix === 'undefined') {
+    global.DOMMatrix = class DOMMatrix {};
+}
+if (typeof global.ImageData === 'undefined') {
+    global.ImageData = class ImageData {};
+}
+if (typeof global.Path2D === 'undefined') {
+    global.Path2D = class Path2D {};
+}
+
 const pdfParse = require('pdf-parse');
 const mammoth = require('mammoth');
 
